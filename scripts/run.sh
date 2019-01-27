0cd73d6278f3e39f30482748b010fab294cceddb
Leonid Muravjev <asm@asm.pp.ru>
2019-01-27 10:19:48 +0300

    fixes

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,13 @@
 set -x
 
 ./iobroker start
-./iobroker add admin --enabled --port 8081
+
+# FIXME: admin.0 isn't work why?
+#        can't set ip 0.0.0.0 for listen
+#        I create admin.1 and start it
+if ! ./iobroker start admin.1 ; then
+	./iobroker add admin --enabled --port 8081
+fi
 
 
 while sleep 10
